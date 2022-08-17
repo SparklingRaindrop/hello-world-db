8e49e4dfbf07a23203b8969e87c64f22bcf235f7
Tsubasa <92432492+SparklingRaindrop@users.noreply.github.com>
2022-08-17 15:40:51 +0200

    Update 20220817095643_first_migration.js

--- migrations/20220817095643_first_migration.js
+++ migrations/20220817095643_first_migration.js
@@ -2,19 +2,17 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-// add
 exports.up = function(knex) {
   return knex.schema.createTable("people", (table) => {
     table.increments("id");
     table.string("name");
-  });
+  })
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-// undo
 exports.down = function(knex) {
-    knex.schema.dropTable('people');
+  return knex.schema.dropTable("people");
 };
