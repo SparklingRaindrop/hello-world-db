81f9c3e73c3e2c8420e1c7cbf2ae2ddee937871d
Tsubasa <92432492+SparklingRaindrop@users.noreply.github.com>
2022-08-17 14:59:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,10 +9,7 @@ app.get("/", (req, res) => {
 
 app.get("/add/:name", async (req, res) => {
   // Ersätt person med den skapade personen från databasen
-  const person = {
-    id: 0,
-    name: "Ryan"
-  };
+
 
   const result = await knex('people').insert({ name: req.params.name });
   res.send(result);
@@ -20,10 +17,7 @@ app.get("/add/:name", async (req, res) => {
 
 app.get("/list", async (req, res) => {
   // Ersätt people med alla personer från databasen
-  const people = [{
-    id: 0,
-    name: "Ryan"
-  }];
+
 
   const result = await knex('people').select();
   res.send(result);
