230b7cca052a7df3e6bf0f6f12397d34e1342d2d
Tsubasa <tsubasa.fujii@cmeducations.se>
2022-08-18 11:31:59 +0200

    Check existence

---
+++ migrations/20220818092620_first_migration.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+ exports.up = function(knex) {
+    knex.schema.hasTable('users').then((exists) => {
+        if (!exists) {
+            return knex.schema.createTable("people", (table) => {
+            table.increments("id");
+            table.string("name");
+            });
+        }
+    });
+};
+  
+  /**
+   * @param { import("knex").Knex } knex
+   * @returns { Promise<void> }
+   */
+exports.down = function(knex) {
+    return knex.schema.dropTable("people");
+};
