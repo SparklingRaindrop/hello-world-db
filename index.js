3424ee4865cd3f3da05776369554ec2fbfdd7a5c
Tsubasa <tsubasa.fujii@cmeducations.se>
2022-08-18 10:24:27 +0200

    Bug fixed

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const app = require("express")();
 
 const config = require("./knexfile.js");
-const knex = require("knex")(/* Knexfile-config */)
+const knex = require("knex")(config[process.env.NODE_ENV] || 'development');
 
 app.get("/", (req, res) => {
   res.send(process.env.GREETING);
 })
 
-app.get("/add/:name", (req, res) => {
+app.get("/add/:name", async (req, res) => {
   // Ersätt person med den skapade personen från databasen
   const person = {
     id: 0,
     name: "Ryan"
   }; 
-
-  res.send(person)
+  const result = await knex('people').insert({ name: req.params.name });
+  res.send(result)
 })
 
 app.get("/list", (req, res) => {
@@ -27,6 +27,6 @@ app.get("/list", (req, res) => {
   res.send(people);
 })
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is listening on port ${process.env.PORT}`)
+app.listen(process.env.PORT || 4000, () => {
+  console.log(`Server is listening on port ${process.env.PORT || 4000}`)
 })
