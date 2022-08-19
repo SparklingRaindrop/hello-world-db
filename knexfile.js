93ee244f6ff4bb9390c9d43504c85f3aaea798e0
Tsubasa <tsubasa.fujii@cmeducations.se>
2022-08-19 09:20:00 +0200

    Revert

--- knexfile.js
+++ knexfile.js
@@ -27,8 +27,7 @@ module.exports = {
     client: 'postgresql',
     connection: process.env.DATABASE_URL,
     migrations: {
-      tableName: 'knex_migrations',
-      directory: './migrations'
+      tableName: 'knex_migrations'
     }
   }
 
